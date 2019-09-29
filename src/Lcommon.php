07272461d1205d1ec3f6550936a9c533ff45b785
wangtjtiejun <13241701929@163.com>
2019-09-29 21:20:47 +0800

    init

---
+++ src/Lcommon.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * User: wangtiejun
+ * Date: 2019/9/29
+ * Time: 21:18
+ */
+namespace Wangtiejun;
+
+class Lcommon
+{
+    /**
+     * 二维数组按照指定字段进行排序
+     * @params array $array 需要排序的数组
+     * @params string $field 排序的字段
+     * @params string $sort 排序顺序标志 SORT_DESC 降序；SORT_ASC 升序
+     */
+    function arraySequence($array, $field, $sort = 'SORT_DESC') {
+        $arrSort = array();
+        foreach ($array as $uniqid => $row) {
+            foreach ($row as $key => $value) {
+                $arrSort[$key][$uniqid] = $value;
+            }
+        }
+        array_multisort($arrSort[$field], constant($sort), $array);
+        return $array;
+    }
+}
